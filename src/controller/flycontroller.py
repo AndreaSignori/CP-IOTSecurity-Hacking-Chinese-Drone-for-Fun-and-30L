9acb2b0482511d67b8a5e30e2ab622f7212fef47
denisG99 <gasparollodenis@gmail.com>
2024-12-22 12:27:58 +0100

    Python documentation

--- src/controller/flycontroller.py
+++ src/controller/flycontroller.py
@@ -6,7 +6,14 @@ from src.controller.heartbeat import Heartbeat
 
 
 class FlyController:
-    def __init__(self, dst_ip: str, dst_port: int, heartbeat_interval: int, heartbeat_msg: bytes, buffer_size: int):
+    def __init__(self, dst_ip: str, dst_port: int, heartbeat_interval: int, heartbeat_msg: bytearray, buffer_size: int):
+        """
+        :param dst_ip: ip address of the target drone
+        :param dst_port: port of the target drone
+        :param heartbeat_interval: time interval between heartbeats in second
+        :param heartbeat_msg: message to send to the target drone as heartbeat
+        :param buffer_size: buffer size for receiving response from the drone
+        """
         self._dst_ip: str = dst_ip
         self._dst_port: int = dst_port
         self._buffer_size: int = buffer_size
@@ -16,6 +23,11 @@ class FlyController:
         self._udp_socket: socket.socket|None = None
 
     def start(self) -> bool:
+        """
+        Start the controller in order to be capable to send and receive messages  and the heartbeat mechanism
+
+        :return: If the controller was started successfully or not
+        """
         if self._udp_socket is None:
             self._udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
@@ -27,17 +39,33 @@ class FlyController:
 
             return True
         return False
+
     def stop(self) -> None:
+        """
+        Stop the controller closing the socket
+        """
+
         self._udp_socket.close()
         self._udp_socket = None
 
-    def send(self, data: bytes) -> int:
+    def send(self, data: bytearray) -> int:
+        """
+        Send the data to the target drone
+
+        :param data: message to send to the target drone
+        :return: the number of bytes sent or -1 in case of the socket is closed
+        """
+
         if not self._udp_socket is None:
             return self._udp_socket.sendto(data, (self._dst_ip, self._dst_port))
         else:
             return -1
 
     def receive(self) -> bytes:
+        """
+        Receive the data from the target drone
+        """
+
         # TODO: valutare meccanismo di timeout
         data, _ = self._udp_socket.recvfrom(self._buffer_size)
 
--- src/controller/heartbeat.py
+++ src/controller/heartbeat.py
@@ -2,7 +2,14 @@ import socket
 import time
 
 class Heartbeat:
-    def __init__(self, udp_conn: socket.socket, dst_ip: str, dst_port: int, msg: bytes, interval: int) -> None:
+    def __init__(self, udp_conn: socket.socket, dst_ip: str, dst_port: int, msg: bytearray, interval: int) -> None:
+        """
+        :param udp_conn: socket used to connect to the target drone
+        :param dst_ip: ip address of the target drone
+        :param dst_port: port of the target drone
+        :param msg: heartbeat message
+        :param interval: interval between heartbeats in second
+        """
         self._socket = udp_conn
         self._dst_ip = dst_ip
         self._dst_port = dst_port
@@ -10,6 +17,10 @@ class Heartbeat:
         self._interval = interval
 
     def send_heartbeat(self) -> None:
+        """
+        Send the heartbeat message every self._interval seconds.
+        """
+
         while True:
             print("Sending heartbeat...")
             self._socket.sendto(self._msg, (self._dst_ip, self._dst_port))
