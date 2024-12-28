060034af0bdf912d39069b7d45e6dc292709e6e7
denisG99 <gasparollodenis@gmail.com>
2024-12-28 10:40:08 +0100

    additional implementation notes

--- src/controller/test_controller.py
+++ src/controller/test_controller.py
@@ -10,6 +10,11 @@ if __name__ == '__main__':
     land_cmd = bytearray([0x3, 0x66, 0x14, 0x80, 0x80, 0x80, 0x80, 0x1, 0x2, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x3, 0x99])
     forward_cmd = bytearray([0x3, 0x66, 0x14, 0x80, 0x80, 0x75, 0x80, 0x0, 0x2, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,0xf7, 0x99])
 
+    """
+    NOTE:
+        * pause after every command is needed in order to give some time to the drone to actuate the command
+        * the command is executed a several time in order to see some concrete action 
+    """
 
     if controller.start():
         controller.send(std_config) # needed in order to establish a connection because enable the possibility to send commands to the drone
@@ -18,7 +23,7 @@ if __name__ == '__main__':
             controller.send(takeoff_cmd)
             time.sleep(0.5)
 
-        time.sleep(2)
+        time.sleep(2) # use to keep the drone in air
 
         for _ in range(10):
             controller.send(std_config)
