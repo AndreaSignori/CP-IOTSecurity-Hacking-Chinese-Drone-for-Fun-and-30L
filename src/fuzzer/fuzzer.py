48869492f860176bd6a90969a3a401a8d24eb140
denisG99 <gasparollodenis@gmail.com>
2024-12-29 21:15:48 +0100

    auto take off and landing

--- src/fuzzer/fuzzer.py
+++ src/fuzzer/fuzzer.py
@@ -42,28 +42,42 @@ def generate_rnd_command() -> bytearray:
 
 if __name__ == "__main__":
     controller = FlyController(dst_ip='192.168.1.1', dst_port=7099, heartbeat_interval=1, heartbeat_msg=bytearray([1, 1]), buffer_size=20)
-    std_config = bytearray([0x3, 0x66, 0x14, 0x80, 0x80, 0x80, 0x80, 0x0, 0x2, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2, 0x99])  # standard position for the controller
+
+    std_config = bytearray([0x3, 0x66, 0x14, 0x80, 0x80, 0x80, 0x80, 0x0, 0x2, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x2, 0x99])
+    takeoff_cmd = bytearray([0x3, 0x66, 0x14, 0x80, 0x80, 0x80, 0x80, 0x1, 0x2, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x3, 0x99])
+    landing_cmd = bytearray([0x3, 0x66, 0x14, 0x80, 0x80, 0x80, 0x80, 0x1, 0x2, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x3, 0x99])
+
     is_power_on = controller.start()
     fuzzer_log_path = "./fuzzer.log"
+
+    # drone take-off
+    for _ in range(10):
+        controller.send(takeoff_cmd)
+        time.sleep(0.5)
 
     while is_power_on:
         fuzz_command = generate_rnd_command()
 
-        controller.send(fuzz_command)
+        if not fuzz_command == takeoff_cmd:
+            controller.send(fuzz_command)
+            time.sleep(0.5)
 
-        drone_heartbeat = controller.receive(wait=1)
+            drone_heartbeat = controller.receive(wait=0.2)
 
-        print(drone_heartbeat)
+            with open(fuzzer_log_path, "a") as f:
+                str = f"{fuzz_command.hex()}\t"
 
-        with open(fuzzer_log_path, "a") as f:
-            str = f"{fuzz_command.hex()}\t"
+                if drone_heartbeat is None:
+                    is_power_on = False # then end the fuzzer execution because we suppose that the drone is unreachable
+                    str += "FAILED\n"
+                else:
+                    str += "OK\n"
 
-            if drone_heartbeat is None:
-                is_power_on = False # then end the fuzzer execution because we suppose that the drone is unreachable
-                str += "FAILED\n"
-            else:
-                str += "OK\n"
+                f.write(str)
 
-            f.write(str)
+    # drone landing
+    for _ in range(10):
+        controller.send(landing_cmd)
+        time.sleep(0.5)
 
     controller.stop()
